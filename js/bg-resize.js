1d25045ad69c0c2f4c3e57ea7c619f924b6c460f
Dirk <hortensd@gmail.com>
2024-02-27 08:01:28 -0800

    Cutting over to css-vh approach to background size locking - will force browser search bar to stay open.

--- js/bg-resize.js
+++
@@ -1,9 +0,0 @@
-/**
-  Mobile URL Bar Fix:
-  https://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
-*/
-var bg = $("#bg","page-container");
-function resizeBackground() {
-    var h = $(window).height();
-    bg.height(h);
-};
