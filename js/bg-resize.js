ab81dae5c997a0c4302ebf1d135b6a94be73a2bb
Dirk <hortensd@gmail.com>
2024-02-27 00:55:14 -0800

    Correcting bg scrolling fix to use a fixed-position div as originally designed. Height resize applied to page-container for parity.

---
+++ js/bg-resize.js
@@ -0,0 +1,10 @@
+/**
+  Mobile URL Bar Fix:
+  https://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
+*/
+var bg = $("#bg","page-container");
+function resizeBackground() {
+    var h = $(window).height();
+    bg.height(h);
+    console.log("H: " + h);
+};
